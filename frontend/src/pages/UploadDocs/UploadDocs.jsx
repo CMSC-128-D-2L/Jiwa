ed2b89950ef43e65c6937a704f1fb204e4f9cdb4
keijeizei <53002096+keijeizei@users.noreply.github.com>
2022-06-07 15:30:28 +0800

    feat: include required units in PUT request

--- frontend/src/pages/UploadDocs/UploadDocs.jsx
+++ frontend/src/pages/UploadDocs/UploadDocs.jsx
@@ -38,6 +38,8 @@ export const UploadDocs = () => {
   // statistics of the upload, from the POST request
   const [uploadReport, setUploadReport] = useState({});
 
+  const [currentReqUnits, setCurrentReqUnits] = useState(0);
+
   useEffect(() => {
     document.body.style.overflow = "unset";
     if (state) {
@@ -138,7 +140,8 @@ export const UploadDocs = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        // console.log(data);
+        setCurrentReqUnits(data.reqUnits);
+
         studentCompleteData = data;
 
         let rows = [];
@@ -357,6 +360,9 @@ export const UploadDocs = () => {
     let { id } = errorStudents[currentErrorStudent];
 
     let student = convertRowsToSems(errorStudentData[currentErrorStudent]);
+
+    student.reqUnits = currentReqUnits;
+
     let payload = new FormData();
     payload.append("NewStudentDetails", JSON.stringify(student));
     payload.append(
